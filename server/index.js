63321d59bc96c6f5b110e83f46c0136508373cc2
alxe_master <alxe.master@gmail.com>
2016-11-22 12:52:03 +0300

    add start scripts

--- server/index.js
+++ server/index.js
@@ -4,21 +4,13 @@ const path = require('path');
 const peer = require('peer');
 const express = require('express');
 
-const DIR_CLIENT = path.resolve(__dirname, '../client');
-const DIR_NODE_MODULES = path.resolve(__dirname, '../node_modules');
+module.exports = (options) => {
+  peer.PeerServer({ port: 9000, path: '/peerjs' });
 
+  const app = express();
 
-peer.PeerServer({ port: 9000, path: '/peerjs' });
+  app.use('/', express.static(path.resolve(__dirname, '../client')));
+  app.use('/node_modules', express.static(path.resolve(__dirname, '../node_modules')));
 
-const app = express();
-
-app.use('/', express.static(DIR_CLIENT));
-app.use('/node_modules', express.static(DIR_NODE_MODULES));
-
-const template = (path) => {
-  delete require.cache[require.resolve(path)];
-
-  return require(path);
+  return app;
 };
-
-app.listen(process.env.NODE_ENV === 'production' ? 80 : 3000);
